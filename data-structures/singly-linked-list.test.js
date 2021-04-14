8dbf75650ee6f505dd675a925c32d232ae1c93fb
Aysia Brown <aysiaebrown@gmail.com>
2021-04-14 14:21:22 -0700

    test suite for singly linked list - all pass

--- data-structures/singly-linked-list.test.js
+++ data-structures/singly-linked-list.test.js
@@ -9,5 +9,43 @@ describe('Singly Linked List', () => {
 
     it('Can instantiate an empty linked list', () => {
         expect(list.head).toBe(null);
-    })
+    });
+
+    it('Can insert a node to the head of an empty linked list',() => {
+        list.insert('cat');
+        expect(list.head.val).toBe('cat');
+    });
+
+    it('Can insert a node to the head of a full linked list', () => {
+        list.insert('cat');
+        list.insert('dog');
+        list.insert('snake');
+        expect(list.head.val).toBe('snake');
+    });
+
+    it('Can append a node to the head of an empty linked list', () => {
+        list.append('dog');
+        expect(list.head.val).toBe('dog');
+    });
+
+    it('Can append a node to the end of a full linked list', () => {
+        list.insert('cat');
+        list.insert('snake');
+        list.append('mouse');
+        expect(list.head.next.next.val).toBe('mouse');
+    });
+
+    it('Can successfully determine if a value is included in the linked list', () => {
+        list.insert('cat');
+        list.insert('dog');
+        list.insert('mouse');
+        expect(list.includes('dog')).toEqual(true);
+    });
+
+    it('Can successfully determine if a value is not included in the linked list', () => {
+        list.insert('cat');
+        list.insert('dog');
+        list.insert('mouse');
+        expect(list.includes('bird')).toEqual(false);
+    });
 })

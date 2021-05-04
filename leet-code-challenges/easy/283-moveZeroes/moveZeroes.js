5fec30b26242531f456c91b4d3b33221bace6610
Aysia Brown <aysiaebrown@gmail.com>
2021-05-04 10:09:49 -0700

    Added prompt to moveZeroes

--- leet-code-challenges/easy/283-moveZeroes/moveZeroes.js
+++ leet-code-challenges/easy/283-moveZeroes/moveZeroes.js
@@ -1,3 +1,12 @@
+// Given an integer array nums, move all 0's to the end of it
+//  while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
 const moveZeroes = function(nums) {
     let zeros = 0;
     let count = 0;
